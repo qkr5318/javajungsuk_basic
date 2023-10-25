519ad062f1c430c65dfcaccddd2ee4c7ccef95c7
박규남 <qkr5318@gmail.com>
2023-10-25 22:34:39 +0900

    2023.10.25 변수_상수_리터럴 과 선언과 저장

---
+++ _ch02/src/VarEx2.java
@@ -0,0 +1,15 @@
+
+
+public class VarEx2 {
+
+		/**
+		 * @param args
+		 */
+		public static void main(String[] args) {
+		final int score = 100;
+//		score = 200;
+		
+		System.out.println(score);
+		
+	}
+}
---
+++ _ch02/src/ex2_1.java
@@ -0,0 +1,16 @@
+
+public class ex2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 행단위로 소스 복사는 ctrl+alt+shift+down
+		// alt+shift+A 컬럼 편집 모드(토글)
+		// print() - 출력 후에 줄바꿈을 안함
+		// println() - 출력 후에 줄바꿈을 한다.
+		System.out.println(5 + 3); // 덧셈
+		System.out.println(5 - 3); // 뺄셈
+		System.out.println(5 * 3); // 곱셈
+		System.out.println(5 / 3.0); // 나눗셈
+	}
+
+}
